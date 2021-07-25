a73845e64b475f09a1b205c8a68244e8db881006
Vishnupriyajeevakumar <vinu.priya0397@gmail.com>
2021-07-25 15:34:03 +0530

    Message of one person

---
+++ src/org/git/GitProject.java
@@ -0,0 +1,28 @@
+package org.git;
+
+public class GitProject {
+	private void name() {
+	System.out.println("Vinu");
+
+	}
+	private void age() {
+		System.out.println("23");
+
+	}
+	private void gender() {
+	System.out.println("Female");
+	}
+	private void address() {
+	System.out.println("Chennai");
+
+	}
+	
+	public static void main(String[] args) {
+		GitProject gp = new GitProject();
+		gp.name();
+		gp.age();
+		gp.gender();
+		gp.address();
+	}
+
+}
